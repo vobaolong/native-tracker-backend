020a7901b4321db2e1d3be6a5fc276a26f510cf9
vobaolong <vobaolong317@gmail.com>
2025-08-25 18:55:46 +0700

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import { ENV } from './config/env.js'
 import { db } from './config/db.js'
 import { favoritesTable } from './db/schema.js'
 import { and, eq } from 'drizzle-orm'
-
+import job from './config/cron.js'
 const app = express()
 const PORT = ENV.PORT || 8001
+
+if (ENV.NODE_ENV === 'production') job.start()
 // app.use(cors())
 app.use(express.json())
 app.get('/api/health', async (req, res) => {
