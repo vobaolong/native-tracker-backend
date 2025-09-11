79e9a2112ae34a43bf7ca9058f4a600c11e56b1e
vobaolong <vobaolong317@gmail.com>
2025-09-11 13:29:19 +0700

    update code

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,4 @@
-import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
 import { ENV } from './env.js'
-import * as schema from '../db/schema.js'
-const sql = neon(ENV.DATABASE_URL)
 
-export const db = drizzle(sql, { schema })
+export const sql = neon(ENV.DATABASE_URL)
--- src/server.js
+++ src/server.js
@@ -1,76 +1,58 @@
 import express from 'express'
 import { ENV } from './config/env.js'
-import { db } from './config/db.js'
-import { favoritesTable } from './db/schema.js'
-import { and, eq } from 'drizzle-orm'
-import job from './config/cron.js'
+import { sql } from './config/db.js'
 const app = express()
 const PORT = ENV.PORT || 8001
 
-if (ENV.NODE_ENV === 'production') job.start()
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+			user_id VARCHAR(255) NOT NULL,
+			title VARCHAR(255) NOT NULL,
+			category VARCHAR(255) NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`
+    console.log('Database initialized successfully')
+  } catch (error) {
+    console.error('Error initializing database:', error)
+    process.exit(1)
+  }
+}
+// if (ENV.NODE_ENV === 'production') job.start()
+
 // app.use(cors())
 app.use(express.json())
-app.get('/api/health', async (req, res) => {
-  res.status(200).json({ success: true })
-})
 
-app.post('/api/favorites', async (req, res) => {
+app.post('/api/transactions', async (req, res) => {
   try {
-    const { userId, recipeId, title, image, cookTime, servings } = req.body
-    if (!userId || !recipeId || !title) {
-      return res.status(400).json({ error: 'Missing required fields' })
+    const { user_id, title, category, amount } = req.body
+    if (!user_id || !title || !category || !amount) {
+      return res.status(400).json({ message: 'Missing required fields' })
     }
-    const newFavorite = await db
-      .insert(favoritesTable)
-      .values({
-        userId,
-        recipeId,
-        title,
-        image,
-        cookTime,
-        servings
-      })
-      .returning()
-
-    res.status(201).json(newFavorite[0])
+    const transaction =
+      await sql`INSERT INTO transactions (user_id, title, category, amount) VALUES (${user_id}, ${title}, ${category}, ${amount}) RETURNING *`
+    console.log(transaction)
+    res.status(201).json(transaction.rows[0])
   } catch (error) {
-    console.error('Error adding favorite:', error)
-    res.status(500).json({ error: 'Failed to add favorite' })
+    console.error('Error creating transaction:', error)
+    res.status(500).json({ message: 'Error creating transaction' })
   }
 })
 
-app.get('/api/favorites/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params
-    const userFavorites = await db
-      .select()
-      .from(favoritesTable)
-      .where(eq(favoritesTable.userId, userId))
-    res.status(200).json(userFavorites)
-  } catch (error) {
-    console.error('Error fetching favorites:', error)
-    res.status(500).json({ error: 'Failed to fetch favorites' })
+app.get('/api/transactions', async (req, res) => {
+  const { user_id } = req.query
+  if (!user_id) {
+    return res.status(400).json({ message: 'Missing user_id' })
   }
+  const transactions =
+    await sql`SELECT * FROM transactions WHERE user_id = ${user_id}`
+  res.status(200).json(transactions.rows)
 })
 
-app.delete('/api/favorites/:userId/:recipeId', async (req, res) => {
-  try {
-    const { userId, recipeId } = req.params
-    await db
-      .delete(favoritesTable)
-      .where(
-        and(
-          eq(favoritesTable.userId, userId),
-          eq(favoritesTable.recipeId, parseInt(recipeId))
-        )
-      )
-    res.status(200).json({ message: 'Favorite removed successfully' })
-  } catch (error) {
-    console.error('Error deleting favorite:', error)
-    res.status(500).json({ error: 'Failed to delete favorite' })
-  }
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+initDB().then(() =>
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+)
